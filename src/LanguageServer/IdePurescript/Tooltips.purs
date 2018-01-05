682f3983d8628efded7b76edc660b38a36d20904
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-01-05 22:20:41 +0000

    Show expanded type in tooltip when it differs from abbreviated one

--- src/LanguageServer/IdePurescript/Tooltips.purs
+++ src/LanguageServer/IdePurescript/Tooltips.purs
@@ -1,20 +1,23 @@
 module LanguageServer.IdePurescript.Tooltips where
 
 import Prelude
+
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff.Class (liftEff)
-import Data.Maybe (Maybe(..))
+import Control.Monad.List.Trans (scanl)
+import Data.Maybe (Maybe(..), fromMaybe, isJust, isNothing)
 import Data.Newtype (un)
 import Data.Nullable (Nullable, toNullable)
 import Data.String (null)
 import IdePurescript.Modules (getQualModule, getUnqualActiveModules)
-import IdePurescript.PscIde (getType)
+import IdePurescript.PscIde (getType, getTypeInfo)
 import IdePurescript.Tokens (identifierAtPoint)
 import LanguageServer.DocumentStore (getDocument)
 import LanguageServer.Handlers (TextDocumentPositionParams)
 import LanguageServer.IdePurescript.Types (ServerState(..), MainEff)
 import LanguageServer.TextDocument (getTextAtRange)
 import LanguageServer.Types (DocumentStore, Hover(..), Position(..), Range(..), Settings, TextDocumentIdentifier(..), markedString)
+import PscIde.Command as C
 
 getTooltips :: forall eff. DocumentStore -> Settings -> ServerState (MainEff eff) -> TextDocumentPositionParams -> Aff (MainEff eff) (Nullable Hover)
 getTooltips docs settings state ({ textDocument, position }) = do
@@ -24,17 +27,23 @@ getTooltips docs settings state ({ textDocument, position }) = do
       char = _.character $ un Position $ position
   case port, identifierAtPoint text char of
     Just port', Just { word, qualifier } -> do
-      ty <- getType port' word modules.main qualifier (getUnqualActiveModules modules $ Just word) (flip getQualModule modules)
-      pure $ toNullable $
-        if null ty then Nothing
-        else Just $ Hover
-            {
-              contents: markedString $ word <> " :: " <> ty
-            , range: toNullable $ Nothing -- Just $ Range { start: position, end: position }
-            }
+      ty <- getTypeInfo port' word modules.main qualifier (getUnqualActiveModules modules $ Just word) (flip getQualModule modules)
+      pure $ toNullable $ map (convertInfo word) ty
     _, _ -> pure $ toNullable Nothing
 
   where
+
+  convertInfo word (C.TypeInfo { type', expandedType }) = Hover
+    {
+      contents: markedString $ compactTypeStr <> 
+        if showExpanded then "\n" <> expandedTypeStr else ""
+    , range: toNullable $ Nothing -- Just $ Range { start: position, end: position }
+    }
+    where
+      showExpanded = isJust expandedType && (expandedType /= Just type')
+      compactTypeStr = word <> " :: " <> type'
+      expandedTypeStr = word <> " :: " <> (fromMaybe "" expandedType)
+
   lineRange (Position { line, character }) =
     Range
       { start: Position
