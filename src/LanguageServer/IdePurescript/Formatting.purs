585cf1359618612df2ef6096a8a1613219db3c44
andys8 <andys8@users.noreply.github.com>
2020-08-23 20:55:06 +0200

    Ignore failure if `purty` not installed

--- src/LanguageServer/IdePurescript/Formatting.purs
+++ src/LanguageServer/IdePurescript/Formatting.purs
@@ -1,9 +1,10 @@
 module LanguageServer.IdePurescript.Formatting where
 
 import Prelude
-import Data.String (null)
-import Effect.Aff (Aff, makeAff)
+import Data.Either (Either(..))
 import Data.Foldable (length)
+import Data.String.Utils (lines)
+import Effect.Aff (Aff, attempt, makeAff)
 import Effect.Class (liftEffect)
 import LanguageServer.DocumentStore (getDocument)
 import LanguageServer.Handlers (DocumentFormattingParams)
@@ -12,18 +13,18 @@ import LanguageServer.TextDocument (getText)
 import LanguageServer.Types (DocumentStore, Position(..), Range(..), Settings, TextDocumentIdentifier(..), TextEdit(..))
 import Node.Buffer (toString)
 import Node.ChildProcess (defaultExecOptions, execFile, stdin)
-import Data.String.Utils (lines)
 import Node.Encoding (Encoding(..))
 import Node.Stream (writeString, end)
 
 getFormattedDocument :: DocumentStore -> Settings -> ServerState -> DocumentFormattingParams -> Aff (Array TextEdit)
 getFormattedDocument docs _settings _serverState { textDocument: TextDocumentIdentifier textDocId } = do
   text <- liftEffect $ getText =<< getDocument docs textDocId.uri
-  newText <- formatWithPurty text
-  if null newText then
-    pure []
-  else
-    pure [ mkTextEdit newText ]
+  newTextEither <- attempt $ formatWithPurty text
+  pure
+    $ case newTextEither of
+        Left _ -> []
+        Right "" -> []
+        Right newText -> [ mkTextEdit newText ]
 
 formatWithPurty :: String -> Aff String
 formatWithPurty text =
