7ec564b5fa4e4f54604594452c9413c85c527071
Alessio Prestileo <alessioprestileo@gmail.com>
2021-05-09 22:49:59 +0200

    Improves clean command by handling correctly non-empty directory

--- src/LanguageServer/IdePurescript/Clean.purs
+++ src/LanguageServer/IdePurescript/Clean.purs
@@ -2,6 +2,7 @@ module LanguageServer.IdePurescript.Clean where
 
 import Prelude
 
+import Data.Array (foldM)
 import Data.Either (Either(..))
 import Effect.Aff (Aff)
 import LanguageServer.IdePurescript.Config (effectiveOutputDirectory)
@@ -16,5 +17,8 @@ clean settings = do
   case isDirectory stats of
     false -> pure $ Left "Could not find output directory"
     true -> do
+      allFiles <- FS.readdir outputDir
+      let allPaths = map (append outputDir) allFiles
+      foldM (\_ b -> FS.unlink b) (mempty) allPaths
       FS.rmdir outputDir
-      pure $ Right $ "Successfully removed directory " <> outputDir
+      pure $ Right $ "Successfully removed directory \"" <> outputDir <> "\""
